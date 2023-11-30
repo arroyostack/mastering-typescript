e1380c33c5a5fd156d7555fd66fc733aa5f2fdd0
arroyostack <arroyostack@gmail.com>
2023-11-30 12:48:39 +0100

    Feat: Union types.

--- src/course-modules/array-types/app.ts
+++ src/course-modules/array-types/app.ts
@@ -1,62 +1,66 @@
-// Two syntax.
-const names: string[] = [];
-const dates: Array<number> = [];
+( () => {
+    // Two syntax.
+    const names: string[] = [];
+    const dates: Array<number> = [];
 
-// Array of arrays.
-const board: string[][] = [
-    ["x", "0", "x"],
-    ["0", "x", "0"],
-    ["0", "x", "0"]
-];
+    // Array of arrays.
+    const board: string[][] = [
+        ["x", "0", "x"],
+        ["0", "x", "0"],
+        ["0", "x", "0"]
+    ];
 
-//  Not common but con be done.
-const secretMessage: number[][][] = [
-    [
-        [1, 4, 6, 3, 8],
-        [3, 5, 3, 2, 4, 2]
-    ],
-    [
-        [3, 54, 34, 34],
-        [4, 5, 6, 34, 5]
-    ]
-];
+    //  Not common but con be done.
+    const secretMessage: number[][][] = [
+        [
+            [1, 4, 6, 3, 8],
+            [3, 5, 3, 2, 4, 2]
+        ],
+        [
+            [3, 54, 34, 34],
+            [4, 5, 6, 34, 5]
+        ]
+    ];
 
-// Exercise
-// **********************************************
-// ******************* PART 1 *******************
-// **********************************************
-// Create an empty array of numbers called "ages":
-const ages: number[] = [];
+    // Exercise
+    // **********************************************
+    // ******************* PART 1 *******************
+    // **********************************************
+    // Create an empty array of numbers called "ages":
+    const ages: number[] = [];
 
-// **********************************************
-// ******************* PART 2 *******************
-// **********************************************
-// Create an array variable called gameBoard that starts as an empty array.
-// It should be typed to hold a 2 dimensional array of strings
-const gameBoard: string[][] = [[]];
+    // **********************************************
+    // ******************* PART 2 *******************
+    // **********************************************
+    // Create an array variable called gameBoard that starts as an empty array.
+    // It should be typed to hold a 2 dimensional array of strings
+    const gameBoard: string[][] = [[]];
 
-// **********************************************
-// ******************* PART 3 *******************
-// **********************************************
-// Create a Product type that contains a name and a price.
-// An example product could be:
-// {name: "coffee mug", price: 11.50}
-type Product = {
-    name: string,
-    price: number;
-};
+    // **********************************************
+    // ******************* PART 3 *******************
+    // **********************************************
+    // Create a Product type that contains a name and a price.
+    // An example product could be:
+    // {name: "coffee mug", price: 11.50}
+    type Product = {
+        name: string,
+        price: number;
+    };
 
-// **********************************************
-// ******************* PART 4 *******************
-// **********************************************
-// Write a function called getTotal that accepts an array of Product types
-// It should return the sum of all the products' prices
-const getTotal = ( productArray: Product[] ) => {
-    let total = 0;
-    for ( let product of productArray ) {
-        total += product.price;
-    }
-    return total;
-};
+    // **********************************************
+    // ******************* PART 4 *******************
+    // **********************************************
+    // Write a function called getTotal that accepts an array of Product types
+    // It should return the sum of all the products' prices
+    const getTotal = ( productArray: Product[] ) => {
+        let total = 0;
+        for ( let product of productArray ) {
+            total += product.price;
+        }
+        return total;
+    };
 
+
+
+} )()
 
